17cfed5964ad1c3dedfe4fcf45f178a23c695db5
Risa Ulinski <Risa@risas-mbp.hamilton.edu>
2018-09-22 13:48:29 -0400

    Initial Commit

---
+++ ChineseCharacterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChineseCharacterApp
+//
+//  Created by Risa Ulinski on 9/22/18.
+//  Copyright © 2018 Hamilton College CS Senior Seminar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ChineseCharacterAppTests/ChineseCharacterAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  ChineseCharacterAppTests.swift
+//  ChineseCharacterAppTests
+//
+//  Created by Risa Ulinski on 9/22/18.
+//  Copyright © 2018 Hamilton College CS Senior Seminar. All rights reserved.
+//
+
+import XCTest
+@testable import ChineseCharacterApp
+
+class ChineseCharacterAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
