9c3265c3669498070437814e0d81f85e26923d64
Risa Ulinski <Risa@risas-macbook-pro.local>
2018-09-25 18:51:19 -0400

    updated buttons.swift

--- ChineseCharacterApp/Buttons.swift
+++ ChineseCharacterApp/Buttons.swift
@@ -38,3 +38,32 @@ class HomeViewButton: UIButton {
     }
     
 }
+
+class ModuleButton: UIButton {
+    
+    
+    required init(value: Int = 0) {
+        // set myValue before super.init is called
+        
+        super.init(frame: .zero)
+        
+        // set other operations after super.init, if required
+        backgroundColor = .white
+    }
+    
+    // For storyboard
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        // If button in storyboard is Custom, you'll need to set
+        // title color for control states and optionally the font
+        // I've set mine to System, so uncomment next three lines if Custom
+        
+        //self.setTitleColor(tintColor, for: .normal)
+        //self.setTitleColor(tintColor.withAlphaComponent(0.3), for: .highlighted)
+        //self.titleLabel?.font = UIFont.systemFont(ofSize: 15.0)
+        //configure()
+        
+        backgroundColor = .white
+    }
+    
+}
