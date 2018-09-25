d6236cde976b1ee66e1de5c66db020fa99ed6e0b
Risa Ulinski <Risa@risas-macbook-pro.local>
2018-09-25 18:15:11 -0400

    Added modules view controller

--- ChineseCharacterApp/ViewControllers/HomeViewController.swift
+++ ChineseCharacterApp/ViewControllers/HomeViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var profileButton: UIButton!
     
     @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var browseButton: UIButton!
+    @IBOutlet weak var modulesButton: UIButton!
+    @IBOutlet weak var quickStartButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
