ceb8da8b54ea876ba90bca8a9ad359a808baf749
Risa Ulinski <Risa@risas-macbook-pro.local>
2018-09-25 14:46:44 -0400

    Added Constant file and renamed first view controller

---
+++ ChineseCharacterApp/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  ChineseCharacterApp
+//
+//  Created by Risa Ulinski on 9/25/18.
+//  Copyright © 2018 Hamilton College CS Senior Seminar. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct AppProperties {
+        static let name = "Chinese Character App"
+    }
+}
