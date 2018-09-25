e1e0a47f50f799058de58f9fdc62c62526d584cc
Risa Ulinski <Risa@risas-macbook-pro.local>
2018-09-25 17:29:49 -0400

    Added colors, fixed small bug with view controller

---
+++ ChineseCharacterApp/ColorScheme.swift
@@ -0,0 +1,24 @@
+//
+//  ColorScheme.swift
+//  ChineseCharacterApp
+//
+//  Created by Risa Ulinski on 9/25/18.
+//  Copyright © 2018 Hamilton College CS Senior Seminar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor  {
+    
+    struct Palette {
+        static let mediumBlue = UIColor(red:0.00, green:0.60, blue:1.00, alpha:1.0)
+        static let lightBlue = UIColor(red:0.86, green:0.93, blue:1.00, alpha:1.0)
+        static let magenta = UIColor(red:0.60, green:0.00, blue:0.80, alpha:1.0)
+        static let lightMagenta = UIColor(red:0.96, green:0.89, blue:1.00, alpha:1.0)
+        
+    }
+    
+    
+
+}
--- ChineseCharacterApp/ViewControllers/HomeViewController.swift
+++ ChineseCharacterApp/ViewControllers/HomeViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
