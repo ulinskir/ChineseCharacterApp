0eb0b9f0e8c82420b9740388a312939ddfb2417f
Risa <Risa@risas-mbp.hamilton.edu>
2018-10-03 00:53:16 -0400

    added module struct

---
+++ ChineseCharacterApp/Module.swift
@@ -0,0 +1,18 @@
+//
+//  Module.swift
+//  ChineseCharacterApp
+//
+//  Created by Risa Ulinski on 10/3/18.
+//  Copyright © 2018 Hamilton College CS Senior Seminar. All rights reserved.
+//
+
+import Foundation
+
+struct Module {
+    var name: String;
+    var chineseChars : [ChineseChar];
+    
+    
+    
+    <#fields#>
+}
