4f79f11bd6abc39aa577585083187c440aa4afea
krashnerd <krishkringle1@gmail.com>
2018-11-10 11:01:17 -0500

    Update ChineseCharacterApp/Character-Recognition/make_svg.py
    Make/tested converter from HTML5 bezier curve to SVG notation

--- ChineseCharacterApp/Character-Recognition/make_svg.py
+++ ChineseCharacterApp/Character-Recognition/make_svg.py
@@ -1,59 +1,80 @@
 import sys
-import JSON
+import re
 
 def scale(points):
-	# Scales from a ((0,0), (500,600)) canvas to a 500x500 canvas
-	# if your project reuires you to understand my code, get
+    # Scales from a ((0,0), (500,600)) canvas to a 500x500 canvas
+    # if your project reuires you to understand my code, get
 
-	NEW_DIM = 500 # Dimensions of standardized canvas
-	IMG_PROP = .9 # Don't want it going all the way to the edge.
+    NEW_DIM = 500 # Dimensions of standardized canvas
+    IMG_PROP = .9 # Don't want it going all the way to the edge.
 
-	max_x = max([x[0] for x in points])
-	min_x = min([x[0] for x in points])
-	max_y = max([y[1] for y in points])
-	min_y = min([y[1] for y in points])
+    max_x = max([x[0] for x in points])
+    min_x = min([x[0] for x in points])
+    max_y = max([y[1] for y in points])
+    min_y = min([y[1] for y in points])
 
-	scalar = IMG_PROP * NEW_DIM / max((max_x - min_x, max_y - min_y))
+    scalar = IMG_PROP * NEW_DIM / max((max_x - min_x, max_y - min_y))
 
-	midpoint = ((max_x + min_x) / 2, (max_y + min_y) / 2)
+    midpoint = ((max_x + min_x) / 2, (max_y + min_y) / 2)
 
-	return [(pt - midpoint[i % 2]) * scalar + NEW_DIM / 2 for i, pt in enumerate(points)]
+    return [(pt - midpoint[i % 2]) * scalar + NEW_DIM / 2 for i, pt in enumerate(points)]
 
-def points_to_SVG(str):
-	pass
+def points_to_SVG(str):
+    pass
 
 class make_ctx:
-	def __init__(self, dest_file):
-		self._stroke_num = 0
-		self._strokes = []
+    def __init__(self):
+        self._stroke_num = 0
+        self._strokes = []
 
-	def curr(self):
-		return self._strokes[-1]
+    def curr(self):
+        return self._strokes[-1]
 
-	def beginPath(self):
-		pass
+    # def beginPath(self):
+    #   pass
 
-	def stroke(self):
-		pass
+    # def stroke(self):
+    #   pass
 
-	def moveTo(self, x, y):
-		self._strokes.append([])
-		self.curr() += ["M", (scale(x)), (scale(y))]
+    def moveTo(self, x, y):
+        self._strokes.append(["M", x, y])
 
-	def bezierCurveTo(self, *argv):
-		self.curr().append('C')
-		for arg in argv:
-			self.curr().append(scale(argv))
+    def bezierCurveTo(self, *argv):
+        self.curr().append('C')
+        for i,arg in enumerate(argv):
+
+            # All but last 2 elements are control points; if not control points, if they're out of bounds throw an error
+            if i <= len(argv) - 2:
+                assert(arg < 500)
+
+            self.curr().append(arg)
+
+    def write_list(self, file):
+        # For each stroke in the list, joins with spaces, to form a list of SVG paths
+        # Write SVG paths to the file.
+        file.write(str([' '.join([str(val) for val in stroke_str])
+                            for stroke_str in self._strokes]))
+        file.write('\n')
 
 def main():
-	args = sys.argv
+    args = sys.argv
 
-	src_filename = args[1]
+    src_filename = args[1]
+    dest_filename = 'strokes.txt'
+    with open(src_filename, 'r') as src_file:
+        with open(dest_filename, 'a') as dest_file:
+            lines = [line[:-2] + '\n' for line in src_file.readlines()]
+            svg_list = make_ctx()
+            for line in lines:
 
-	with src_path  = open(src_filename, 'r'):
-		with dest_path = open(dest_filename, 'a'):
-			lines = src_file.readlines()
-			ctx = make_ctx()
-			for line in lines[1:-1]:
-				eval(line, {'ctx':ctx, 'xoff':0, 'yoff':0})
-		
+                #Cleansing input
+                if not re.match('^\s*ctx[.](moveTo|bezierCurveTo)', line):
+                    continue
+
+                # Evaluate a call of a ctx method with ctx as '
+                eval(line, {'ctx':svg_list, 'xoff':0, 'yoff':0})
+            svg_list.write_list(dest_file)
+
+if __name__ == '__main__':
+    main()
+
