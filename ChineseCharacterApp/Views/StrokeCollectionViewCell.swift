f9a0fcd7873997c23de48080553634b8e86307c4
Risa <Risa@risas-mbp.hamilton.edu>
2018-11-28 00:17:01 -0500

    set up drawing view controller as collection delegate and data source

---
+++ ChineseCharacterApp/Views/StrokeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  StrokeCollectionViewCell.swift
+//  ChineseCharacterApp
+//
+//  Created by Risa Ulinski on 11/28/18.
+//  Copyright © 2018 Hamilton College CS Senior Seminar. All rights reserved.
+//
+
+import UIKit
+
+class StrokeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var strokeView: UIView!
+}
